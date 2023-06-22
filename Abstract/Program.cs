64789808d36aeebc72c9c254b869e11b91eff57e
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-06-22 12:47:30 +0530

    Add project files.

---
+++ Abstract/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle(10);
+            c1.CalculateArea();
+            Console.WriteLine(c1);
+
+            Rectangle r1 = new Rectangle(10, 20);
+            r1.CalculateArea();
+            Console.WriteLine(r1);
+        }
+    }
+}
---
+++ Abstract/Shape.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    abstract public class Shape
+    {
+        public abstract void CalculateArea();
+    }
+    public class Circle : Shape
+    {
+        private int r;
+        private double area;
+        public Circle(int r)
+        {
+            this.r = r;
+        }
+        public override void CalculateArea()
+        {
+            area = 3.14 * r * r;
+        }
+        public override string ToString()
+        {
+            return $"Area is{area}";
+        }
+    }
+    public class Rectangle : Shape {
+        private int l;
+        private int b;
+        private double area;
+        public Rectangle(int l, int b)
+        {
+            this.l = l;
+            this.b = b;
+        }
+        public override void CalculateArea()
+        {
+            area = l * b;
+        }
+        public override string ToString()
+        {
+            return $"Area is {area}";
+        }
+    }
+}
